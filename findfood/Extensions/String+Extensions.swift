00477da6968dfc731cc6ad49ddcb970cfdc3b1ac
Bertay Yonel <bertayyonel@gmail.com>
2023-07-20 23:18:47 +0300

    semantic-fixes-2

--- findfood/Extensions/String+Extensions.swift
+++ findfood/Extensions/String+Extensions.swift
@@ -7,37 +7,12 @@
 
 import Foundation
 
-public extension CharacterSet {
-    
-    static let urlQueryParameterAllowed = CharacterSet.urlQueryAllowed.subtracting(CharacterSet(charactersIn: "&?"))
-
-    static let urlQueryDenied           = CharacterSet.urlQueryAllowed.inverted()
-    static let urlQueryKeyValueDenied   = CharacterSet.urlQueryParameterAllowed.inverted()
-    static let urlPathDenied            = CharacterSet.urlPathAllowed.inverted()
-    static let urlFragmentDenied        = CharacterSet.urlFragmentAllowed.inverted()
-    static let urlHostDenied            = CharacterSet.urlHostAllowed.inverted()
-
-    static let urlDenied                = CharacterSet.urlQueryDenied
-        .union(.urlQueryKeyValueDenied)
-        .union(.urlPathDenied)
-        .union(.urlFragmentDenied)
-        .union(.urlHostDenied)
 
 
-    func inverted() -> CharacterSet {
-        var copy = self
-        copy.invert()
-        return copy
-    }
-}
-
-public extension String {
+extension String {
     func urlEncoded(denying deniedCharacters: CharacterSet = .urlDenied) -> String? {
         return addingPercentEncoding(withAllowedCharacters: deniedCharacters.inverted())
     }
-}
-
-extension String {
     static var empty: String {
         ""
     }
